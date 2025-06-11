e6bf833248c857024547aafbbeeee7f673382c5e
Amal A M <vectorhero007@gmail.com>
2025-06-11 10:15:58 +0530

    ThemeData.dark().copyWith(..........)

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,9 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         appBarTheme: AppBarTheme(color: Color(0xFF090C22)),
         scaffoldBackgroundColor: Color(0xFF090C22),
-        textTheme: TextTheme(bodyMedium: TextStyle(color: Colors.white)),
       ),
       debugShowCheckedModeBanner: false,
       home: InputPage(),
@@ -32,18 +31,8 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('BMI CALCULATOR', style: TextStyle(color: Colors.white)),
-        ),
-      ),
+      appBar: AppBar(title: Center(child: Text('BMI CALCULATOR'))),
       body: Center(child: Text('body')),
-      floatingActionButton: Theme(
-        data: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        ),
-        child: FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-      ),
     );
   }
 }
